971a71ef62dbdd46979073e75c916e0d79e2fed5
Aleksander Sushchenko <039pro@gmail.com>
2022-11-27 22:25:51 +0200

    разобрадся с созданием добавил связи не получается загрузить данный в базу Исправил, заполнил Все работает с заполнением Ура закончил!!!!!!

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ class Book(Base):
     title = Column('title', String(length=40))
     id_publ = Column('id_publ', Integer, ForeignKey('publisher.id_publ'))
 
+    def __repr__(self):
+        return f'{self.title}'
+
+
     def __init__(self, id_book, title, id_publ):
         self.id_book = id_book
         self.title = title
@@ -38,6 +42,10 @@ class Shop(Base):
     def __init__(self, id_shop, name):
         self.id_shop = id_shop
         self.name = name
+
+    def __repr__(self):
+        return f'{self.name}'
+
 
 
 class Stock(Base):
@@ -69,6 +77,10 @@ class Sale(Base):
         self.id_stock = id_stock
         self.count = count
 
+    def __repr__(self):
+        return f'{self.price} | {self.date_sale}'
+
+
 
 engine = create_engine('postgresql://postgres:1109@localhost:5432/dbalchemy')
 # Base.metadata.drop_all(bind=engine)
@@ -76,58 +88,63 @@ Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+#
+# publ_1 = Publisher(1,'Пушкин')
+# publ_2 = Publisher(2,'Чехов')
+# publ_3 = Publisher(3, 'Толстой')
+#
+# session.add(publ_1)
+# session.add(publ_2)
+# session.add(publ_3)
+# session.commit()
+#
+# book_1 = Book(1, 'Капитанская дочь', 1)
+# book_2 = Book(2, 'Руслан и Людмида', 1)
+# book_3 = Book(3, 'Война и Мир', 3)
+# book_4 = Book(4, 'Вишневый сад', 2)
+#
+# session.add(book_1)
+# session.add(book_2)
+# session.add(book_3)
+# session.add(book_4)
+# session.commit()
+#
+# shop_1 = Shop(1, 'Буквоед')
+# shop_2 = Shop(2, 'Книги и Книжечки')
+#
+# session.add(shop_1)
+# session.add(shop_2)
+#
+# session.commit()
+#
+# stock_1 = Stock(1, 1, 1, 1)
+# stock_2 = Stock(2, 2, 1, 1)
+# stock_3 = Stock(3, 3, 2, 1)
+# stock_4 = Stock(4, 4, 2, 1)
+#
+# session.add(stock_1)
+# session.add(stock_2)
+# session.add(stock_3)
+# session.add(stock_4)
+#
+# session.commit()
+#
+# sale_1 = Sale(1, 300, '11.09.2021', 1, 1)
+# sale_2 = Sale(2, 200, '11.09.2021', 2, 1)
+# sale_3 = Sale(3, 100, '11.09.2021', 3, 1)
+# sale_4 = Sale(4, 150, '11.09.2021', 4, 1)
+#
+# session.add(sale_1)
+# session.add(sale_2)
+# session.add(sale_3)
+# session.add(sale_4)
+#
+# session.commit()
+zapros = input()
+result = session.query(Book, Shop, Sale).filter(Publisher.name == zapros).filter(Publisher.id_publ == Book.id_publ).filter(Book.id_publ == Stock.id_book).filter(Stock.id_shop == Shop.id_shop).filter(Stock.id_stock == Sale.id_stock).all()
+for r in result:
+    print(f'{r[0]} | {r[1]} | {r[2]}')
 
-publ_1 = Publisher(1,'Пушкин')
-publ_2 = Publisher(2,'Чехов')
-publ_3 = Publisher(3, 'Толстой')
-
-session.add(publ_1)
-session.add(publ_2)
-session.add(publ_3)
-session.commit()
-
-book_1 = Book(1, 'Капитанская дочь', 1)
-book_2 = Book(2, 'Руслан и Людмида', 1)
-book_3 = Book(3, 'Война и Мир', 3)
-book_4 = Book(4, 'Вишневый сад', 2)
-
-session.add(book_1)
-session.add(book_2)
-session.add(book_3)
-session.add(book_4)
-session.commit()
-
-
-shop_1 = Shop(1, 'Буквоед')
-shop_2 = Shop(2, 'Книги и Книжечки')
-
-session.add(shop_1)
-session.add(shop_2)
-
-session.commit()
-
-stock_1 = Stock(1, 1, 1, 1)
-stock_2 = Stock(2, 2, 1, 1)
-stock_3 = Stock(3, 3, 2, 1)
-stock_4 = Stock(4, 4, 2, 1)
-
-session.add(stock_1)
-session.add(stock_2)
-session.add(stock_3)
-session.add(stock_4)
-
-session.commit()
-
-sale_1 = Sale(1, 300, '11.09.2021', 1, 1)
-sale_2 = Sale(2, 200, '11.09.2021', 2, 1)
-sale_3 = Sale(3, 100, '11.09.2021', 3, 1)
-sale_4 = Sale(4, 150, '11.09.2021', 4, 1)
-
-session.add(sale_1)
-session.add(sale_2)
-session.add(sale_3)
-session.add(sale_4)
-
-session.commit()
 
 session.close()
+
